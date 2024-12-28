0f06ba0cbc794b6517aded70faf882fffa47cbbe
Tarun Kumar <130552075+tarrrrun@users.noreply.github.com>
2024-12-28 19:45:29 +0530

    My first package implementation in java

---
+++ JavaRevision/src/PackageImplementation.java
@@ -0,0 +1,10 @@
+import mypack1.Demo;
+public class PackageImplementation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo dd=new Demo();
+		dd.display();
+	}
+
+}
