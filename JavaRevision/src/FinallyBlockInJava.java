047f3b83e0d1cd1fb81cfb46ccf19507a86fc06b
Tarun Kumar <130552075+tarrrrun@users.noreply.github.com>
2025-01-01 18:27:06 +0530

    Stack Implementation using Arrays.

--- JavaRevision/src/FinallyBlockInJava.java
+++ JavaRevision/src/FinallyBlockInJava.java
@@ -2,13 +2,21 @@
 
 
 public class FinallyBlockInJava {
-
+	static int meth1() {
+		try {
+			System.out.println("xyz");
+			return 5/1;
+		}finally {
+			System.out.println("finally");//Executes before returning in try block
+		}
+	}
+	
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
-			
+			System.out.println(meth1());
 		try
 		{
-			System.out.println(5/0);
+			System.out.println(5/2);
 		}catch(ArithmeticException e) {
 			System.out.println(e.toString());
 		}
@@ -16,7 +24,7 @@ public class FinallyBlockInJava {
 			System.out.println("finally block");
 			
 		}
-		
+	
 		
 		
 	}
