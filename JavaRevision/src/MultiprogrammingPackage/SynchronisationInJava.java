059532ead468e58ebe7a7843f6b672caa4bb1ed4
Tarun Kumar <130552075+tarrrrun@users.noreply.github.com>
2025-01-03 00:51:09 +0530

    Simple Synchronization

---
+++ JavaRevision/src/MultiprogrammingPackage/SynchronisationInJava.java
@@ -0,0 +1,46 @@
+package MultiprogrammingPackage;
+class MyData
+{
+	synchronized void disp(String str) {
+
+			for (int i=0;i<str.length();i++)
+			{
+				System.out.println(str.charAt(i));
+			}
+		
+	}
+}
+class MyThread1 extends Thread
+{
+	MyData d;
+	MyThread1(MyData dat){
+		d=dat;
+	}
+	public void run()
+	{
+		d.disp("Hello World");
+	}
+}
+class MyThreadd2 extends Thread
+{
+	MyData d;
+	MyThreadd2(MyData dat)
+	{
+		d=dat;
+	}
+	public void run() {
+		d.disp("Welcome");
+	}
+}
+public class SynchronisationInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyData d=new MyData();
+		MyThread1 mt1=new MyThread1(d);
+		MyThreadd2 mt2=new MyThreadd2(d);
+		mt1.start();
+		mt2.start();
+	}
+
+}
