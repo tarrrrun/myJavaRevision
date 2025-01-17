67492fd0bca5e10e6563b9853dc8bcdaabe1d2b8
Tarun Kumar <130552075+tarrrrun@users.noreply.github.com>
2025-01-17 16:47:03 +0530

    methodss of thread

--- JavaRevision/src/MultiprogrammingPackage/ThreadMethodsInJava.java
+++ JavaRevision/src/MultiprogrammingPackage/ThreadMethodsInJava.java
@@ -1,19 +1,36 @@
 package MultiprogrammingPackage;
+
+class MyThreaddd extends Thread
+{
+	public MyThreaddd(String name)
+	{
+		super(name);
+	}
+	public void run() {
+		int i=1;
+		while(i<10) {
+			System.out.println("This thread: "+i++);
+			try{Thread.sleep(200);}catch(Exception e) {System.out.println(e);}
+		}
+		
+	}
+}
+
 class MyThreadd extends Thread{
 	public MyThreadd(String name) {
 		super(name);
 		//setPriority(Thread.MIN_PRIORITY+2);
 
-	}
+	} 
 	public void run() {
 		int i = 1;
         while (true) {
             System.out.println(i + " !!!!");
-//            try {
-//            	Thread.sleep(600);
-//            }catch(InterruptedException e){
-//            	System.out.println(e.toString());
-//            }
+            try {
+            	Thread.sleep(600);
+            }catch(InterruptedException e){
+            	System.out.println(e.toString());
+            }
             i++;
         }
 	}
@@ -22,6 +39,14 @@ public class ThreadMethodsInJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			MyThreaddd mth= new MyThreaddd("Thread1 new");
+			System.out.println("NameL: "+mth.getName());
+			System.out.println("priority: "+mth.getPriority());
+			System.out.println("State: "+mth.getState());
+			mth.setPriority(Thread.MAX_PRIORITY-2);
+			mth.start();
+			System.out.println("Alive: "+mth.isAlive());
+			mth.interrupt();
 			MyThreadd t=new MyThreadd("My Thread 1");
 			t.setPriority(Thread.NORM_PRIORITY+2);
 			//System.out.println(t.getId());		This method is deprecated.
